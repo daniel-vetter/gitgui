ddc0567948e38af54d5d54b196dba91afe2c0bb2
Daniel Vetter <daniel.vetter86@gmail.com>
2017-01-22 17:56:23 +0100

    Increased indent size

--- src/app/src/main.js
+++ src/app/src/main.js
@@ -10,33 +10,33 @@ const spawn = require('child_process').spawn;
 let mainWindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 1200, height: 800, show: false })
-  mainWindow.webContents.on("did-finish-load", () => { mainWindow.show(); })
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-  mainWindow.setMenu(null);
-  if (process.argv.indexOf("--devtools") !== -1) {
-    mainWindow.webContents.openDevTools({ mode: "undocked" });
-  }
+    mainWindow = new BrowserWindow({ width: 1200, height: 800, show: false })
+    mainWindow.webContents.on("did-finish-load", () => { mainWindow.show(); })
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+    mainWindow.setMenu(null);
+    if (process.argv.indexOf("--devtools") !== -1) {
+        mainWindow.webContents.openDevTools({ mode: "undocked" });
+    }
 
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
 }
 
 app.on('ready', createWindow)
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
