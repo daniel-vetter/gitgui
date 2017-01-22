a6c05d9120c645c241f113f9b1ee106a45a5897b
Daniel Vetter <daniel.vetter86@gmail.com>
2017-01-22 05:03:57 +0100

    Added build script which creates a releasable package

---
+++ build.cmd
@@ -0,0 +1,17 @@
+@echo off
+cd src\app
+cmd /c ng build -prod -o ..\..\build\app
+cd ..\..
+node src\app\node_modules\electron-packager\cli.js ^
+     build\app GitGui ^
+     --platform=win32 ^
+     --electron-version=1.4.15 ^
+     --out=build\dist ^
+     --icon=src\app\icon.ico ^
+     --app-copyright="Daniel Vetter 2017" ^
+     --win32metadata.CompanyName="Daniel Vetter" ^
+     --win32metadata.ProductName="GitGui" ^
+     --win32metadata.OriginalFilename="GitGui.exe" ^
+     --win32metadata.FileDescription="GitGui" ^
+     --asar ^
+     --overwrite
