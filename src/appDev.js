856cb267ac1d702d9b8358cd4049eedb2e699059
Daniel Vetter <daniel.vetter86@gmail.com>
2017-02-01 04:39:34 +0100

    Fixed linux development setup

---
+++ src/appDev.js
@@ -0,0 +1,12 @@
+const spawn = require('child_process').spawn;
+
+if (process.platform === "linux") {
+    spawn("gnome-terminal", ["-e", "node node_modules/angular-cli/bin/ng build -w"], { cwd: "./app" });
+    spawn("gnome-terminal", ["-e", "node node_modules/webpack/bin/webpack.js -w"], { cwd: "./main" });
+}
+else if (process.platform === "win32") {
+    spawn("cmd", ["/c", "start node node_modules/angular-cli/bin/ng build -w"], { cwd: "./app" });
+    spawn("cmd", ["/c", "start node node_modules/webpack/bin/webpack.js -w"], { cwd: "./main" });
+} else {
+    console.log("platform not supported");
+}
