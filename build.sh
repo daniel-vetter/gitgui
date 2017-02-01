5392af759d95fb379bbb070d64b12662ed02712e
Daniel Vetter <daniel.vetter86@gmail.com>
2017-02-01 02:51:53 +0100

    Added linux buildscript

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm -rf ./build
+cd src/app
+npm install
+node node_modules/angular-cli/bin/ng build -prod -o
+cd ..
+cd main
+npm install
+node node_modules/webpack/bin/webpack.js
+cd ../..
+node src/app/node_modules/electron-packager/cli.js \
+    build/app \
+    GitGui \
+    --platform=linux \
+    --electron-version=1.4.15 \
+    --out=build/dist \
+    --icon=src/app/icon.ico \
+    --app-copyright="Daniel Vetter 2017" \
+    --win32metadata.CompanyName="Daniel Vetter" \
+    --win32metadata.ProductName="GitGui" \
+    --win32metadata.OriginalFilename="GitGui.exe" \
+    --win32metadata.FileDescription="GitGui" \
+    --asar \
+    --overwrite
