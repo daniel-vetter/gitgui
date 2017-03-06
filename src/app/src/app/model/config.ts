7baa63cd73130b661cf2ba4fc2818203c38ff800
Daniel Vetter <daniel.vetter86@gmail.com>
2017-03-06 02:50:46 +0100

    Changed default icon pack

--- src/app/src/app/model/config.ts
+++ src/app/src/app/model/config.ts
@@ -1,6 +1,5 @@
 export class ConfigData {
     loadLastRepositoryOnStartup: boolean = false;
     recentRepositories: string[] = [];
-    //fileIconPackageUrl = "https://github.com/EmmanuelBeziat/vscode-great-icons.git";
-    fileIconPackageUrl = "https://github.com/PKief/vscode-extension-material-icon-theme.git";
+    fileIconPackageUrl = "https://github.com/jtlowe/studio-icons.git";
 }
