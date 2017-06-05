d5dc4c90c6719031d0f2d1028fb72c9ec792b2d2
Daniel Vetter <daniel.vetter86@gmail.com>
2017-06-05 02:34:38 +0200

    Added small debug tool

--- src/app/src/app/app.component.ts
+++ src/app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ErrorHandler  } from "@angular/core";
+import { Component, OnInit, ErrorHandler } from "@angular/core";
 import { ThemeManager } from "./services/theme-manager";
 import { MenuManager } from "./menu/menu-manager";
 import { Config } from "./services/config";
@@ -25,6 +25,7 @@ export class AppComponent implements OnInit {
         this.themeManager.init();
         this.fileIconManager.init();
         this.loadLastRepository();
+        window["startDomMonitor"] = this.domChangeMonitor;
     }
 
     private async loadLastRepository() {
@@ -32,6 +33,37 @@ export class AppComponent implements OnInit {
         if (recentRepositories !== undefined && recentRepositories.length > 0) {
             this.repositoryOpener.open(recentRepositories[recentRepositories.length - 1]);
         }
+    }
+
+
+    private domChangeMonitor() {
+        var observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation: MutationRecord) => {
+                if (mutation.type === "attributes" && (mutation.attributeName === undefined || mutation.attributeName.startsWith("ng-")))
+                    return;
+                if (mutation.target instanceof Comment)
+                    return;
+
+                if (mutation.type === "attributes" && mutation.oldValue !== undefined && mutation.attributeName !== undefined) {
+                    const attr = mutation.target.attributes.getNamedItem(mutation.attributeName)
+                    if (attr)
+                        mutation["newValue"] = attr.value
+                }
+                
+                console.log(mutation);
+            });
+        });
+
+        var config = {
+            attributes: true,
+            childList: true,
+            characterData: true,
+            attributeOldValue: true,
+            characterDataOldValue: true,
+            subtree: true
+        };
+
+        observer.observe(document.body, config);
     }
 }
 
