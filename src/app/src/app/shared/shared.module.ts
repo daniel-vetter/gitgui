cb5dc49fd97d11aaa21d67496ca8d7a7845df502
Daniel Vetter <daniel.vetter86@gmail.com>
2017-06-11 18:31:37 +0200

    Loading component added

--- src/app/src/app/shared/shared.module.ts
+++ src/app/src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { ProfileImageComponent } from "./profile-image/profile-image.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { TreeViewComponent } from "./tree-view/tree-view.component";
 import { CheckBoxComponent } from "./check-box/check-box.component";
+import { LoadingComponent } from "./loading/loading.component";
 
 @NgModule({
     declarations: [
         ProfileImageComponent,
         TreeViewComponent,
-        CheckBoxComponent
+        CheckBoxComponent,
+        LoadingComponent
     ],
     imports: [
         BrowserModule
