612a0049be88f88183304936d2815f923211363a
Daniel Vetter <daniel.vetter86@gmail.com>
2017-07-12 02:41:36 +0200

    Build script bootstrapper cleanup

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
-cmd /c "cd .\src\build && npm install"
-cmd /c "cd .\src\build && npm run build"
+cmd /c "cd .\src\build && npm install" > NUL
+cmd /c "cd .\src\build && npm run build" > NUL
 echo Build script compiled
 node .\src\build\build\build.js
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-(cd src/build; npm install)
-(cd src/build; npm run build)
+(cd src/build; npm install) > /dev/null
+(cd src/build; npm run build) > /dev/null
 echo Build script compiled
 node ./src/build/build/build.js
